1c6f4f7fd45f031244f4da099a8ff31b9613ad82
unknown <agami.malikov@gmail.com>
2021-04-12 20:34:28 +0300

    wawe

--- js/main.js
+++ js/main.js
@@ -1,22 +1,30 @@
-$(function() {
-  
+$(function () {
+
   $(".menu a").on("click", function (event) {
-		event.preventDefault();
-		var id  = $(this).attr('href'),
-			top = $(id).offset().top;
-		$('body,html').animate({scrollTop: top}, 1500);
-	});
+    event.preventDefault();
+    var id = $(this).attr('href'),
+      top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
 
   $('.blog__slider').slick({
     arrows: false,
     dots: true,
   })
-  
-  $('.menu__btn, .menu__link').on('click', function(){
+
+  $('.menu__btn, .menu__link').on('click', function () {
     $('.header__inner').toggleClass('header__inner--active');
   });
-  
-  
-  var mixer = mixitup('.gallery__photos'); 
-  
+
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 50) {
+      $('.header__inner').addClass('header__inner--fixed');
+    }
+    else {
+      $('.header__inner').removeClass('header__inner--fixed');
+    }
+  });
+
+  var mixer = mixitup('.gallery__photos');
+
 });
